0f1cd582715e930537be0675384b66e424bcb898
Marcos Monti <marcos.monti@globant.com>
2014-08-12 12:10:42 -0300

    added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name = 'django-inline-sortable',
+    version = '0.0.1',
+    packages = ['isortable', ],
+    author = 'Marcos Monti',
+    author_email = 'marcosmonti@gmail.com',
+    url = 'http://github.com/marcosmonti/django-inline-sortable/',
+    license = 'GPLv3',
+    description = 'Django app used for ordering inline formset elements using drag&drop',
+    long_description = 'Django app used for ordering inline formset elements using drag&drop.',
+    keywords = 'admin inline sortable python',
+    install_requires = [],
+)
